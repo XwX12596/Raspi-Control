5512099423e8dde5feef3797304be515addb63ae
XwX12596 <1259685860@qq.com>
2023-08-07 09:36:40 +0100

    del unnecessary NN class

--- classifier/test12.py
+++ classifier/test12.py
@@ -7,39 +7,6 @@ from torch.utils.data import DataLoader, TensorDataset
 from torchvision import datasets, transforms
 
 def test12(input):
-    class Network(nn.Module):
-        def __init__(self, layers_dim1, layers_dim2, input_dim, output_dim):
-            super(Network, self).__init__()
-            Layers1 = OrderedDict()
-            for layer_i in range(len(layers_dim1) - 1):
-                Layers1["linear_{}".format(layer_i)] = nn.Linear(layers_dim1[layer_i], layers_dim1[layer_i + 1])
-                if layer_i != len(layers_dim1) - 2:
-                    Layers1["relu_{}".format(layer_i)] = nn.ReLU()
-                if layer_i == len(layers_dim1) - 1:
-                    Layers1["sigmoid_{}".format(layer_i)] = nn.Sigmoid()
-
-            Layers2 = OrderedDict()
-            for layer_i in range(len(layers_dim2) - 1):
-                Layers2["linear_{}".format(layer_i)] = nn.Linear(layers_dim2[layer_i], layers_dim2[layer_i + 1])
-                if layer_i != len(layers_dim2) - 2:
-                    Layers2["relu_{}".format(layer_i)] = nn.ReLU()
-                if layer_i == len(layers_dim2) - 1:
-                    Layers2["sigmoid_{}".format(layer_i)] = nn.Sigmoid()
-
-            self.bp_net_1 = nn.Sequential(Layers1)
-            self.bp_net_2 = nn.Sequential(Layers2)
-            self.input_dim = input_dim
-            self.output_dim = output_dim
-            for n in self.modules():
-                if isinstance(n, nn.Linear):
-                    nn.init.xavier_normal_(n.weight.data)
-                    if n.bias is not None:
-                        nn.init.constant_(n.bias, 0.0)
-
-        def forward(self, data):
-            return self.bp_net_1(data), self.bp_net_2(data)
-
-
     cl_net = torch.load('/home/xmh/raspi-control/classifier/model_12.pth')
 
     data_real= np.array(input)
